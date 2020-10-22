12c9c11a1d5a6b9234cc6f2273bf6c6a59e0cdd0
sanketb <sanketb@stud.ntnu.no>
2020-10-22 22:06:34 +0200

    Started on neural network

--- NEAT_cartpole.py
+++ NEAT_cartpole.py
@@ -1,15 +1,40 @@
 class Genome:
-    def __init__(self, nodes, genes):
-        self.
+    def __init__(self, nodes, edge):
+        self.nodes = nodes
+        self.edge = edge
+
+
+    def findBestNode():
+        nodeList=[]
+
+        outputnode_1 = Node(1,0,0)
+        outputnode_2 = Node(2,0,0)
+        nodeList.append(outputnode_1)
+        nodeList.append(outputnode_2)
+
+        for i in range(0, len(edge)-1):
+            if(Genome.edge[i].output.index == outputnode_1.index):
+                outputnode_1.nodeValue += edge[i].weight*edge[i].input.nodeValue
+            else:
+                outputnode_2.nodeValue += edge[i].weight*edge[i].input.nodeValue
+        if(nodeList[0].nodeValue > nodeList[1].nodeValue):
+            return nodeList[0].nodeValue
+        else:
+            return nodeList[1].nodeValue
+
+
+
     
 # Nodes
 class Node:
-    def __init__(self, node, innov):
-        self.node = node
+    def __init__(self, index, nodeValue, innov):
+        #self.node = node
+        self.index = index
         self.innov = innov
+        self.nodeValue = nodeValue
 
 # Vertecies
-class Gene:
+class Edge:
     def __init__(self, in_node, out_node, weight, enabled, innov):
         # Input node
         self.input = in_node
